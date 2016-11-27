42228d453cded46398326e46b9e2671522b1714b
Nick Hird <nickdh94@gmail.com>
2016-11-27 04:15:06 +0000

    added tile textures

--- src/Level/Tile.java
+++ src/Level/Tile.java
@@ -2,8 +2,15 @@ package Level;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 
 import Controller.Camera;
+import Utils.ContentLoader;
+
+import javax.imageio.ImageIO;
 
 public class Tile {
 	
@@ -15,25 +22,34 @@ public class Tile {
 	
 	private boolean visible;
 	private Color color;
-	
 	private char ch;
+	private BufferedImage bufferedImage;
 	
-	public Tile(boolean col, boolean vis, double gravity, double friction, Color color, char c) {
+	public Tile(boolean col, boolean vis, double gravity, double friction, Color color, char c, String tilePath) {
 		collidable = col;
 		visible = vis;
 		this.gravity = gravity;
 		this.friction = friction;
 		this.color = color;
 		this.ch = c;
-	}
+		if (!tilePath.isEmpty()) {
+            try {
+                bufferedImage = ImageIO.read(new FileInputStream(tilePath));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 	
 	public void draw(Graphics2D g, Camera c, int x, int y) {
 		//DRAW TILE
-		//if (visible) {
-			g.setColor(color);
-			g.fillRect(x * TILE_SIZE - (int) c.x, y * TILE_SIZE - (int) c.y, TILE_SIZE, TILE_SIZE);
+		if (visible) {
+			g.drawImage(bufferedImage, x * TILE_SIZE - (int) c.x, y * TILE_SIZE - (int) c.y, TILE_SIZE, TILE_SIZE, null);
 			//g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
-		//}
+		}else {
+			g.setColor(new Color(200, 200, 255));
+			g.fillRect(x * TILE_SIZE - (int) c.x, y * TILE_SIZE - (int) c.y, TILE_SIZE, TILE_SIZE);
+		}
 	}
 	
 	public char getChar() {
